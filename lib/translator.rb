5410afef1e21311620e12228c186815e7a797254
Chana Kupetz <chanakupetz@gmail.com>
2020-08-11 04:47:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,18 +22,7 @@ def get_english_meaning(file_path, emoticon)
   emoticon.each do |key, emoticon_pairs|
     if emoricon_pairs[1] == japanese_emoticon
       return key
-
     end
-
 end
-
-
-def get_english_meaning(file_path, japanese_emoticon)
- emoticons = YAML.load_file('./lib/emoticons.yml')
-  load_library(file_path)
-
- emoticons.each do |key, emoticon_pairs|
-
-  if emoticon_pairs[1] == japanese_emoticon
-   return key
- end
+  return "Sorry, that emoticon was not found"
+end
