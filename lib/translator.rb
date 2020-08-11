c77422134dad019fc4c445da343a88267bd3e134
Chana Kupetz <chanakupetz@gmail.com>
2020-08-11 04:47:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon
 end
 
 def get_english_meaning(file_path, emoticon)
-  hash = load_library(file_path)
+  load_library(file_path)
   emoticon.each do |key, emoticon_pairs|
     if emoricon_pairs[1] == japanese_emoticon
       return key
