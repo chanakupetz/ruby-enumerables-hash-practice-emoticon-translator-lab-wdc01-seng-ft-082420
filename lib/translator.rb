280ff340b1fd65ea8ad597084b95d9e38d81042e
Chana Kupetz <chanakupetz@gmail.com>
2020-08-11 05:15:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
    end
 end
 return "Sorry, that emoticon was not found"
-end	
+end
 
 #def get_japanese_emoticon(file_path, english_emoticon)
 #  emoticons = YAML.load_file('./lib/emoticons.yml')
