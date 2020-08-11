1220bab35c97de403f0bfaa3afddb1cfef71581c
Chana Kupetz <chanakupetz@gmail.com>
2020-08-11 04:48:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon
 end
 
 def get_english_meaning(file_path, emoticon)
-  emoticons = YAML.load_file('./lib/emoticons.yml')
+  emoticon = YAML.load_file('./lib/emoticons.yml')
  load_library(file_path)
 
   emoticon.each do |key, emoticon_pairs|
