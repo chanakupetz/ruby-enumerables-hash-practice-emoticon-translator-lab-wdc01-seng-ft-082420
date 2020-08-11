88f0dc2aa7f09cf66eb57d2ef275985ba0d5e2ae
Chana Kupetz <chanakupetz@gmail.com>
2020-08-11 05:14:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,16 +13,30 @@ def load_library(file_path)
 end
 
 
-def get_japanese_emoticon(file_path, japanese_emoticon)
+def get_japanese_emoticon(file_path, english_emoticon)
   emoticon = YAML.load_file('./lib/emoticons.yml')
  load_library(file_path)
- emoticon.each do |key, emoticon_pairs|
-   if emoticon_pairs[0] == english_emoticon
-     binding.pry
 
+ emoticon.each do |emoticon_pairs|
+   if emoticon_pairs[0] == english_emoticon
      return emoticon_pairs[1]
    end
 end
+return "Sorry, that emoticon was not found"
+end	
+
+#def get_japanese_emoticon(file_path, english_emoticon)
+#  emoticons = YAML.load_file('./lib/emoticons.yml')
+#  load_library(file_path)
+  #emoticon_pair_arrays = emoticons.values
+
+  #emoticon_pair_arrays.each do |pair_array|
+
+   #if pair_array[0] == english_emoticon
+     #return pair_array[1]
+  #  end
+  #end
+
 
 def get_english_meaning(file_path, japanese_emoticon)
   emoticon = YAML.load_file('./lib/emoticons.yml')
